5353cc9bc3cb38ab559feeca60a9f598fd023b0d
Rafael de Abreu Gonçalves <66883350+Rafaeldag@users.noreply.github.com>
2020-09-09 17:21:40 -0300

    Update cachemactrix.R

--- cachemactrix.R
+++ cachemactrix.R
@@ -1,3 +1,8 @@
+## makeCacheMatrix will generate a matrix and cache its inverse, while cacheSolve checks to see if the inverse has already been calculated.
+## If it has it will pull it from the cache, and if not it will calculate the inverse.
+
+## This function generates the starting matrix, creates the cache, and defines the get, setinverse, and getinverse functions
+
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
   set <- function(y){
@@ -10,6 +15,8 @@ makeCacheMatrix <- function(x = matrix()) {
   list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
+## This function checks to see if the inverse has been calculated already. If it has, it will return the inverse from the cache with a message. If it hasn't 
+## it will calculate the inverse and use the setinverse function from the makeCacheMatrix environment to load the inverse of the matrix to the environment.
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
